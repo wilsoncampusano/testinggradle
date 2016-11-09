0a666d52d68bef3e0b11905682126cb4b7d1fdf9
Wilson Campusano <wicampusano@arshumano.com>
2016-11-09 16:44:13 -0400

    task and compile java plugin

---
+++ src/main/java/irg/HelloWorld.java
@@ -0,0 +1,11 @@
+package main.java.irg;
+
+/**
+ * Created by wicampusano on 11/9/2016.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
